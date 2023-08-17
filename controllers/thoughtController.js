185e202256f37c222727f09751455d5be5c0c28d
BondT1 <tombond248@gmail.com>
2023-08-17 19:40:59 +0100

    new thought and update thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,16 +7,51 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
-getSingleThought(req, res) {
-  Thought.findOne({ _id: req.params.thoughtId })
-    .select("-__v")
-    .then((thought) =>
-      !thought
-        ? res.status(404).json({ message: "Thought ID invalid" })
-        : res.json(thought)
-    )
-    .catch((err) => res.status(500).json(err));
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Thought ID invalid" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
   },
+
+  newThought(req, res) {
+    Thought.create(req.body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Thought ID invalid" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, New: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "Thought ID invalid" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  
+
+
 
 
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,87 +2,87 @@ const { User, Thought } = require("../models");
 
 module.exports = {
 
-getUsers(req, res) {
-  User.find({})
-    .then((user) => res.json(user))
-    .catch((err) => res.status(500).json(err));
-},
+  getUsers(req, res) {
+    User.find({})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
 
-getSingleUser(req, res) {
-  User.findOne({ _id: req.params.userId })
-    .populate("thoughts")
-    .populate("friends")
-    .select("-__v")
-    .then((user) =>
-      !user
-        ? res.status(404).json({ message: "User ID invalid" })
-        : res.json(user)
-    )
-    .catch((err) => res.status(500).json(err));
-},
-
-newUser(req, res) {
-  User.create(req.body)
-    .then((user) => res.json(user))
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).json(err);
-    });
-},
-
-updateUser(req, res) {
-  User.findOneAndUpdate(
-    { _id: req.params.userId },
-    { $set: req.body },
-    { runValidators: true, new: true }
-  )
-    .then((user) =>
-      !user
-        ? res.status(404).json({ message: "User ID invalid" })
-        : res.json(user)
-    )
-    .catch((err) => res.status(500).json(err));
-},
-
-removeUser(req, res) {
-  User.findOneAndDelete({ _id: req.params.userId })
-    .then((user) =>
-      !user
-        ? res.status(404).json({ message: "User ID invalid" })
-        : Thought.deleteMany({ _id: { $in: user.thoughts } })
-    )
-    .then(() => res.json({ message: "User and Thought removed" }))
-    .catch((err) => res.status(500).json(err));
-},
-
-addFriend(req, res) {
-  User.findOneAndUpdate(
-    { _id: req.params.userId },
-    { $addToSet: { friends: req.params.friendId } },
-    { runValidators: true, new: true }
-  )
-    .then((user) =>
-      !user
-        ? res.status(404).json({ message: "User ID invalid" })
-        : res.json(user)
-    )
-    .catch((err) => res.status(500).json(err));
-},
-
-removeFriend(req, res) {
-  User.findOneAndUpdate(
-    { _id: req.params.userId },
-    { $pull: { friends: req.params.friendId } },
-    { new: true }
-  )
-    .then(
-      (user) =>
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((user) =>
         !user
           ? res.status(404).json({ message: "User ID invalid" })
           : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  newUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
     )
-    .catch((err) => res.status(500).json(err));
-},
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "User ID invalid" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  removeUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "User ID invalid" })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: "User and Thought removed" }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "User ID invalid" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then(
+        (user) =>
+          !user
+            ? res.status(404).json({ message: "User ID invalid" })
+            : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
 }
 
